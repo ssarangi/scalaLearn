c3f6879b25b78288fd9491d679868ed93ceb905f
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2015-07-16 22:53:56 -0700

    Added Vector class finally working

---
+++ src/main/scala/linalg/Matrix.scala
@@ -0,0 +1,10 @@
+package linalg
+
+/**
+ * Created by sarangis on 7/13/15.
+ * Adapted from this blog: https://medium.com/@eob/how-you-might-create-a-scala-matrix-library-in-a-functional-programming-style-760f8bf6ee6
+ * as well as http://etrain.github.io/2015/05/28/type-safe-linear-algebra-in-scala/
+ */
+class Matrix {
+
+}
---
+++ src/main/scala/linalg/Vector.scala
@@ -0,0 +1,43 @@
+package linalg
+
+import scala.specialized
+
+/**
+ * Created by sarangis on 7/13/15.
+ */
+
+import Numeric.Implicits._
+import Fractional.Implicits._
+
+
+trait BinOps[T] {
+  def +(that: Vector[T])(implicit ev: Numeric[T]): Vector[T]
+  def -(that: Vector[T])(implicit ev: Numeric[T]): Vector[T]
+  def *(that: Vector[T])(implicit ev: Numeric[T]): Vector[T]
+  def /(that: Vector[T])(implicit ev: Fractional[T]): Vector[T]
+}
+
+trait Vector[T] {
+  def toList(): List[T]
+  def zeros(length: Int)(implicit ev: Numeric[T]): Vector[T]
+}
+
+
+object Vector {
+  def apply[T](args: T*): Vector[T] = new VectorImpl[T](args.toList)
+
+  private class VectorImpl[@specialized(Double, Int, Float, Long)T](val _data: List[T])
+    extends Vector[T]
+    with BinOps[T] {
+    def +(that: Vector[T])(implicit ev: Numeric[T]): Vector[T] = new VectorImpl[T](_data.zip(that).map(elem => elem._1 + elem._2))
+    def -(that: Vector[T])(implicit ev: Numeric[T]): Vector[T] = new VectorImpl[T](_data.zip(that).map(elem => elem._1 - elem._2))
+    def *(that: Vector[T])(implicit ev: Numeric[T]): Vector[T] = new VectorImpl[T](_data.zip(that).map(elem => elem._1 * elem._2))
+    def /(that: Vector[T])(implicit ev: Fractional[T]): Vector[T] = new VectorImpl[T](_data.zip(that).map(elem => elem._1 / elem._2))
+
+    def fill(length: Int, value: T): Vector[T] = new VectorImpl[T](List.fill[T](length)(value))
+    def zeros(length: Int)(implicit ev: Numeric[T]): Vector[T] = fill(length, ev.zero)
+    def toList(): List[T] = _data.toList
+  }
+
+  implicit def VectorToList[T](v: Vector[T]): List[T] = v.toList
+}
