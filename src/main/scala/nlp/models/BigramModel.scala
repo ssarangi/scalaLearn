3c297435dc3f6f6af5c195dc6449893107bbe9df
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2015-08-02 21:08:02 -0700

    Added language models

---
+++ src/main/scala/nlp/models/BigramModel.scala
@@ -0,0 +1,10 @@
+package nlp.models
+
+/**
+ * Created by sarangis on 7/29/15.
+ */
+class BigramModel(str: String) extends LanguageModel {
+  def analyze(): Unit = {
+
+  }
+}
---
+++ src/main/scala/nlp/models/LanguageModel.scala
@@ -0,0 +1,8 @@
+package nlp.models
+
+/**
+ * Created by sarangis on 7/29/15.
+ */
+trait LanguageModel {
+
+}
